d887976e2d5ae1405e22357d966ff8c0e28bc7af
Eldan Ben-Haim <eldan@transmitsecurity.com>
2017-09-23 10:32:37 +0300

    Moving to std python

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-# Copyright 2017, Google, Inc.
+# Copyright 2016 Google Inc.
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#    http://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -11,13 +12,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# [START app_yaml]
-runtime: python27
-api_version: 1
-threadsafe: true
+import webapp2
 
-handlers:
-- url: /.*
-  script: main.app
 
-# [END app_yaml]
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write('Hello, World!')
+
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+], debug=True)
